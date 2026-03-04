1ee461b29748a601c57263903be39a735480133d
Ganesh Yadav <ganeshyadav342342@gmail.com>
2026-03-04 17:58:52 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,10 @@ import AdminLayout from './components/layers/admin/AdminLayout';
 import PrivateRoute from './auth/PrivateRoute';
 import Login from './auth/Login';
 import Register from './auth/Register';
-import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Dashboard from './pages/dashboard/Dashboard';
+
+// Masters
 import ModuleMst from './pages/masters/ModuleMst';
 import SubmoduleMst from './pages/masters/SubmoduleMst';
 import Activities from './pages/masters/Activities';
@@ -25,13 +27,9 @@ import OpdBillMaster from "./pages/masters/OpdBillMaster";
 import OpdBillingDetails from "./pages/masters/OpdBillingDetails";
 import OpdBilling from "./pages/masters/OpdBilling";
 
-import { ThemeProvider } from './theme/ThemeContext.jsx';
-import { adminRoutes } from './routes/routeConfig';
-
-// Masters
 import IcdMasterMst from "./pages/masters/IcdMasterMst.jsx";
 import RoomTypeMasterMst from "./pages/masters/RoomTypeMasterMst";
-import BedMaster from "./pages/masters/BedMaster"; 
+import BedMaster from "./pages/masters/BedMaster";
 import HabitMaster from "./pages/masters/HabitMaster";
 import HallucinationMaster from "./pages/masters/HallucinationMaster";
 import HistoryMaster from "./pages/masters/HistoryMaster";
@@ -45,8 +43,6 @@ import MedicineCategory from './pages/masters/MedicineCategory';
 import MedicineMst from './pages/masters/MedicineMst';
 import AppointmentTypeMasterMst from './pages/masters/AppointmentTypeMasterMst';
 import AppointmentMst from './pages/masters/AppointmentMst';
-// import TransactionsMst from './pages/masters/TransactionsMst';
-import { ThemeProvider } from './theme/ThemeContext.jsx'; 
 
 import ComplaintMaster from './pages/masters/ComplaintMaster.jsx';
 import ExpensesMst from './pages/masters/ExpensesMst.jsx';
@@ -58,52 +54,51 @@ import StatesMst from './pages/masters/StatesMst.jsx';
 import DistrictsMst from './pages/masters/DistrictsMst.jsx';
 import CitiesMst from './pages/masters/CitiesMst.jsx';
 import DoctorMst from './pages/masters/DoctorMst.jsx';
-import { ThemeProvider } from './theme/ThemeContext.jsx'; 
-import DoctorMst from './pages/masters/DoctorMst.jsx'
-import EctMst from './pages/masters/EctMst.jsx'
-import FollowUpMst from './pages/masters/FollowUpMst.jsx'
+import EctMst from './pages/masters/EctMst.jsx';
+import FollowUpMst from './pages/masters/FollowUpMst.jsx';
 
-
-
+import { ThemeProvider } from './theme/ThemeContext.jsx';
 
 function App() {
   return (
-    <div className="app-container">
-      <Routes>
-        {/* Auth Routes */}
-        <Route path="/admin/login" element={<Login />} />
-        <Route path="/admin/register" element={<Register />} />
+    <ThemeProvider>
+      <div className="app-container">
+        <Routes>
 
-        {/* Protected Admin Routes */}
-        <Route 
-          path="/admin" 
-          element={
-            <PrivateRoute>
-              <AdminLayout />
-            </PrivateRoute>
-          }
-        >
-          {/* Example of nested dynamic routes inside AdminLayout */}
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="countries" element={<Countries />} />
-          <Route path='moduleMst' element={<ModuleMst />} />
-          <Route path='submoduleMst' element={<SubmoduleMst />} />
-          <Route path='activities' element={<Activities />} />
-          <Route path='advice_master' element={<AdviceMaster />} />
-          <Route path="company_master" element={<CompanyMaster />} />
-          <Route path="employee_master" element={<EmployeeMaster />} />
-          <Route path='marital_status_master' element={<MaritalStatusMaster />} />
-          <Route path='relation_master' element={<RelationMaster />} />
-          <Route path="departments" element={<Departments />} />
-          <Route path="blood_group_master" element={<BloodGroupMaster />} />
-          <Route path="blood_donor" element={<BloodDonor />} />
-          <Route path="bankdetails" element={<BankDetails />} />
-          <Route path="bed_allotment" element={<BedAllotment />} />
-          <Route path="patient" element={<Patient />} />
-          <Route path="opd-bill-master" element={<OpdBillMaster />} />
-          <Route path="opd-billing-details" element={<OpdBillingDetails />} />
-          <Route path="opd-billing" element={<OpdBilling />} />
-            {/* Masters (all under /admin) */}
+          {/* Auth Routes */}
+          <Route path="/admin/login" element={<Login />} />
+          <Route path="/admin/register" element={<Register />} />
+
+          {/* Protected Admin Routes */}
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute>
+                <AdminLayout />
+              </PrivateRoute>
+            }
+          >
+            <Route path="dashboard" element={<Dashboard />} />
+
+            <Route path="moduleMst" element={<ModuleMst />} />
+            <Route path="submoduleMst" element={<SubmoduleMst />} />
+            <Route path="activities" element={<Activities />} />
+            <Route path="advice_master" element={<AdviceMaster />} />
+            <Route path="company_master" element={<CompanyMaster />} />
+            <Route path="employee_master" element={<EmployeeMaster />} />
+            <Route path="marital_status_master" element={<MaritalStatusMaster />} />
+            <Route path="relation_master" element={<RelationMaster />} />
+            <Route path="departments" element={<Departments />} />
+            <Route path="blood_group_master" element={<BloodGroupMaster />} />
+            <Route path="blood_donor" element={<BloodDonor />} />
+            <Route path="bankdetails" element={<BankDetails />} />
+            <Route path="bed_allotment" element={<BedAllotment />} />
+            <Route path="patient" element={<Patient />} />
+            <Route path="opd-bill-master" element={<OpdBillMaster />} />
+            <Route path="opd-billing-details" element={<OpdBillingDetails />} />
+            <Route path="opd-billing" element={<OpdBilling />} />
+
+            {/* Masters */}
             <Route path="icd-master" element={<IcdMasterMst />} />
             <Route path="room-type-master" element={<RoomTypeMasterMst />} />
             <Route path="bed" element={<BedMaster />} />
@@ -131,21 +126,18 @@ function App() {
             <Route path="mse" element={<MseMaster />} />
             <Route path="thought_content_master" element={<ThoughtContentMaster />} />
             <Route path="noticeboard" element={<Noticeboard />} />
-            <Route path="ect" element={<EctMst/>} />
-            <Route path="follow-up" element={<FollowUpMst/>} />
-            
+            <Route path="ect" element={<EctMst />} />
+            <Route path="follow-up" element={<FollowUpMst />} />
 
           </Route>
 
-          {/* Root Redirect */}
-          {/* Root */}
+          {/* Root redirect */}
           <Route path="/" element={<Navigate to="/admin/login" replace />} />
 
         </Routes>
       </div>
     </ThemeProvider>
   );
-}
 }
 
 export default App;
